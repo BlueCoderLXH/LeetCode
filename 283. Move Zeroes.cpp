edd4a3856b22c60c591ea40a349fa1cd347f06f3
BlueCoderLXH <bluecoder@yeah.net>
2016-04-09 16:18:23 +0800

    Create 283. Move Zeroes.cpp

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+		int zeroCount = 0;
+		int size = (int)nums.size();
+
+		// 移动非零数到最左边
+		for (int i = 0; i < size; i++)
+		{
+			if (nums[i] == 0)
+			{
+				zeroCount++;
+			}
+			else
+			{
+				nums[i - zeroCount] = nums[i];
+			}
+		}
+
+		// 零复制到最右边
+		for (int i = size - 1; i >= size - zeroCount; i--)
+		{
+			nums[i] = 0;
+		}
+    }
+};
