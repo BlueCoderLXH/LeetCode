7f0157d9cbed8772396b204cccfda082b933cdbe
BlueCoderLXH <bluecoder@yeah.net>
2016-05-27 12:49:32 +0800

    Update 347. Top K Frequent Elements.cpp

--- 347. Top K Frequent Elements.cpp
+++ 347. Top K Frequent Elements.cpp
@@ -1,5 +1,10 @@
 /*
-    时间复杂度: O(nlogk)
+    算法思路：基于桶排序思想, 先用一个elementCountMap统计每一个不同元素出现的次数, 以元素值为key,
+元素出现次数为value, 相当于一个桶. 再用一个countElementMap, 以元素出现次数为key, 元素值为value. 
+countElementMap中按照元素出现次数排序(默认是升序), 然后从后往前遍历countElementMap, 遍历k次就得到
+top-k元素.
+    时间复杂度：O(nlogk)
+    n为初始数组元素的个数, k为不同元素的个数, 显然k<n. 于是O(nlogk) < O(nlogn), 满足题目要求.
 */
 class Solution {
 public:
